53e3d9e7098d90398b25c7417ff56ce04b534736
RudekAlone <44509433+RudekAlone@users.noreply.github.com>
2023-12-22 09:45:22 +0100

    Update LoginView.php
    Fix acction rename /login to /Logowanie

--- Views/Auth/LoginView.php
+++ Views/Auth/LoginView.php
@@ -4,7 +4,7 @@ class LoginView
     public function render()
     {
         echo <<<HTML
-        <form method="POST" action="/login">
+        <form method="POST" action="/Logowanie">
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
             <button type="submit">Login</button>
